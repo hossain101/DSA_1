9764aa4be7cf8cfa593ea6a9ba14a8710ffaab64
shoaib <shoaibhossain24@gmail.com>
2023-04-06 02:33:40 +0600

    Part 1 & Part 2 of course

--- src/Earth.java
+++ src/Earth.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Earth {
+public class Earth {
+
+    public static void main(String[] args) {
+      Human tom;
+      tom  = new Human();
+      tom.age = 5;
+      tom.heightInInches = 80;
+      tom.eyeColor = "blue";
+      tom.name = "Tom Hossain";
+
+        Human Shoaib = new Human("Shoaib", 24, 90, "piercing-brown");
+
+      tom.speak();
+        System.out.println("\n\n");
+
+        Shoaib.speak();
+    }
 }
